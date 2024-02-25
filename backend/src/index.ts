73e37e13c3ab71176bfe1bc47f7a1c6250b00f8d
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2024-02-25 12:29:19 +0530

    Fixed wrangler.toml | | user signup and signin endpoints

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,33 +16,49 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-app.post('/api/v1/signup', async(c) => {
+
+app.post('/api/v1/signup', async (c) => {
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL	,
+	}).$extends(withAccelerate());
+
+	const body = await c.req.json();
+	try {
+		const user = await prisma.user.create({
+			data: {
+				email: body.email,
+				password: body.password
+			}
+		});
+		const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+		return c.json({ jwt });
+	} catch(e) {
+		c.status(403);
+		return c.json({ error: "error while signing up" });
+	}
+})
+
+app.post('/api/v1/signin', async(c) => {
+
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
-}).$extends(withAccelerate())
+  }).$extends(withAccelerate())
 
   const body = await c.req.json()
-
-  const user  = await prisma.user.create({
-    data:{
-      email: body.email,
-      password: body.password,
+  const user = await prisma.user.findUnique({
+    where: {
+      email: body.email
     }
+  
   })
-
-  const secret = 'mySecretKey'
-  const token = await sign({id: user.id}, c.env.JWT_SECRET );
-
-
-return c.json({
-  jwt:token
-
-})
-
-
+    if(!user) {
+      c.status(403)
+      return c.json({ error: "user not found" })
+    }
+    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET)
+    return c.json({ jwt })
 
 })
-app.post('/api/v1/signin', (c) => {return c.text('Hello Hono Post signup !')})
 app.post('/api/v1/blog', (c) => {return c.text('Hello Hono Post signupblog!')})
 app.put('/api/v1/blog', (c) => {return c.text('Hello Hono Post signup blog !')})
 app.get('/api/v1/blog/:id', (c) => {return c.text('Hello Hono Post signup !')})
